087635ffe5f61e4684c990f779a5cad2eaf41394
Nici Putter <niciputter@yahoo.com>
2019-02-19 22:10:10 +0100

    All the win, lose and draw conditions set for players

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -6,10 +6,17 @@ describe('game', function () {
     });
 
     describe('the game is a draw if', function ()  {
+        it('should return "draw" on "rock", "rock"', function () {
+            expect(game('rock', 'rock')).to.equal('draw');
+        });
+
         it('should return "draw" on "paper", "paper"', function () {
             expect(game('paper', 'paper')).to.equal('draw');
         });
-    
+
+        it('should return "draw" on "scissors", "scissors"', function () {
+            expect(game('scissors', 'scissors')).to.equal('draw');
+        });
     });
 
     describe('player_1 wins if', function ()   {
@@ -17,5 +24,27 @@ describe('game', function () {
             expect(game('rock', 'scissors')).to.equal('Player 1 wins');
         });
 
+        it('should return "Player 1 wins" on "paper", "rock"', function () {
+            expect(game('paper', 'rock')).to.equal('Player 1 wins');
+        });
+
+        it('should return "Player 1 wins" on "scissors", "paper"', function () {
+            expect(game('scissors', 'paper')).to.equal('Player 1 wins');
+        });
+    });
+
+
+    describe('player_2 wins if', function ()   {
+        it('should return "Player 2 wins" on "scissors", "rock"', function () {
+            expect(game('scissors', 'rock')).to.equal('Player 2 wins');
+        });
+
+        it('should return "Player 2 wins" on "rock", "paper"', function () {
+            expect(game('rock', 'paper')).to.equal('Player 2 wins');
+        });
+
+        it('should return "Player 2 wins" on "paper", "scissors"', function () {
+            expect(game('paper', 'scissors')).to.equal('Player 2 wins');
+        });
     });
 
--- src/js/game.js
+++ src/js/game.js
@@ -3,10 +3,12 @@ module.exports = function (player_1_choice, player_2_choice)  {
         return 'draw';
     }
  
-    if (player_1_choice === 'rock' && player_2_choice === 'scissors')  {
-        return 'Player 1 wins';
-    }
-    
+    if ((player_1_choice === 'rock' && player_2_choice === 'scissors')  ||
+        (player_1_choice === 'scissors' && player_2_choice === 'paper') ||
+        (player_1_choice === 'paper' && player_2_choice === 'rock'))
+            return 'Player 1 wins';
+        else 
+            return 'Player 2 wins';
 };
 
 
