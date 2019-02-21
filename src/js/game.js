7dbf09f505853dd173c463086fddf1d9f337a0f9
Nici Putter <niciputter@yahoo.com>
2019-02-21 22:08:41 +0100

    HTML/DOM elements introduced in .js and updated HTML file

--- src/js/game.js
+++ src/js/game.js
@@ -1,28 +1,52 @@
-module.exports = function (player_choice, pc_choice)  {
-    
-    function roll_dice() {
-        var dice = Math.random();
-        if (dice <= 0.33) {
-            pc_choice = "rock";
-        } 
-        else if (dice > 0.33 && dice <= 0.66) {
-            pc_choice = "paper";
-        } 
-        else {
-            pc_choice = "scissors";
-        }
-     }
 
+
+var rock = document.getElementById('rock').addEventListener('click', run);
+var paper = document.getElementById('paper').addEventListener('click', run);
+var scissors = document.getElementById('scissors').addEventListener('click', run);
+
+var pc_choice;
+var player_choice;
+
+var player = document.getElementById('player');
+var computer = document.getElementById('computer');
+var outcome = document.getElementById('outcome');
+
+
+function run() {
+    console.log("clicked")
+    player_choice = this.innerText;
+    computerGamble();
+    compare();
+}
+
+function computerGamble() {
+    var dice = Math.random();
+  if (dice <= 0.33) {
+        pc_choice = "Rock";
+  } else if (dice > 0.33 && dice <= 0.66) {
+        pc_choice = "Paper";
+  } else {
+        pc_choice = "Scissors";
+  }
+}
+
+function compare() {
+   player.innerHTML = "You have chosen " + player_choice;
+   computer.innerHTML = "Computer chooses " + pc_choice;
+    var result;
     if (player_choice === pc_choice) {
-        return 'It is a draw';
+        result = 'It is a draw';
+        print
     }
- 
-    if ((player_choice === 'rock' && pc_choice === 'scissors')  ||
-        (player_choice === 'scissors' && pc_choice === 'paper') ||
-        (player_choice === 'paper' && pc_choice === 'rock'))
-            return 'Yay, you win!';
+    console.log(player_choice.toLowerCase())
+    if ((player_choice.toLowerCase() === 'rock' && pc_choice.toLowerCase() === 'scissors')  ||
+        (player_choice.toLowerCase() === 'scissors' && pc_choice.toLowerCase() === 'paper') ||
+        (player_choice.toLowerCase() === 'paper' && pc_choice.toLowerCase() === 'rock'))
+            result = 'Yay, you win!';
         else 
-            return 'Sorry, you lose';
+            result = 'Sorry, you lose';
+        
+        outcome.innerHTML = result    
 };
 
 
