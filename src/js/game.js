483ede5259b988425df7e2e0d7a94612084aac49
Nici Putter <niciputter@yahoo.com>
2019-02-22 09:35:48 +0100

    Sanitized code formatting

--- src/js/game.js
+++ src/js/game.js
@@ -1,19 +1,13 @@
-
-
-var rock = document.getElementById('rock').addEventListener('click', run);
-var paper = document.getElementById('paper').addEventListener('click', run);
-var scissors = document.getElementById('scissors').addEventListener('click', run);
-
+const rock = document.getElementById('rock').addEventListener('click', run);
+const paper = document.getElementById('paper').addEventListener('click', run);
+const scissors = document.getElementById('scissors').addEventListener('click', run);
 var pc_choice;
 var player_choice;
-
 var player = document.getElementById('player');
 var computer = document.getElementById('computer');
 var outcome = document.getElementById('outcome');
 
-
 function run() {
-    console.log("clicked")
     player_choice = this.innerText;
     computerGamble();
     compare();
@@ -31,15 +25,14 @@ function computerGamble() {
 }
 
 function compare() {
-   player.innerHTML = "You have chosen " + player_choice;
-   computer.innerHTML = "Computer chooses " + pc_choice;
+    player.innerHTML = "You choice is " + player_choice;
+    computer.innerHTML = "Computer choice is " + pc_choice;
     var result;
     if (player_choice === pc_choice) {
         result = 'It is a draw';
-        print
-    }
-    console.log(player_choice.toLowerCase())
-    if ((player_choice.toLowerCase() === 'rock' && pc_choice.toLowerCase() === 'scissors')  ||
+        console.log(player_choice.toLowerCase())
+        
+    } else if ((player_choice.toLowerCase() === 'rock' && pc_choice.toLowerCase() === 'scissors')  ||
         (player_choice.toLowerCase() === 'scissors' && pc_choice.toLowerCase() === 'paper') ||
         (player_choice.toLowerCase() === 'paper' && pc_choice.toLowerCase() === 'rock'))
             result = 'Yay, you win!';
